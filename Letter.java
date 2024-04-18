e05d34f6db756552dd7ed09a2e47e2730fd0c15e
Ujjwal kant <131240250+Ujjwalkant01@users.noreply.github.com>
2024-04-18 16:49:05 +0530

    Add files via upload

---
+++ Letter.java
@@ -0,0 +1,9 @@
+// How many letter in the String.
+public class Letter {
+    public static void main(String[] args){
+        String name="Ujjwal";
+        int value= name.length();//Calculate the string length
+        System.out.println(value);
+
+    }
+}
