8f3464d15c076701aa55fbe189db77ee9a08b619
Ujjwal kant <131240250+Ujjwalkant01@users.noreply.github.com>
2024-04-20 16:37:19 +0530

    Add files via upload

---
+++ array.java
@@ -0,0 +1,21 @@
+public class array {
+    public static void main(String[] args){
+
+        int [] marks = new int[6];
+
+        marks[0]=21;
+        marks[1]=22;
+        marks[2]=23;
+        marks[3]=24;
+        marks[4]=55;
+        marks[5]=67;
+
+        //find length of an array
+        System.out.println(marks.length);
+
+        //print the marks of 3rd index value
+        System.out.println(marks[3]);
+
+
+    }
+}
