5e5ebcc459a838d27441fef278fe1576c2e4a15d
Ujjwal kant <131240250+Ujjwalkant01@users.noreply.github.com>
2024-04-23 18:12:14 +0530

    Add files via upload

---
+++ Practise.java
@@ -0,0 +1,10 @@
+public class Practise {
+    public static void main(String[] args){
+        int a=1;
+        do{
+            System.out.println(a);
+            a++;
+        }
+        while(a<5);
+    }
+}
