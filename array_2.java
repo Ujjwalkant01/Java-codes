f91ad14d97185e59725f2b5d5fed2b3d0d061c6b
Ujjwal kant <131240250+Ujjwalkant01@users.noreply.github.com>
2024-04-24 10:20:05 +0530

    Add files via upload

---
+++ array_2.java
@@ -0,0 +1,12 @@
+public class array_2 {
+    public static void main(String[] args){
+
+        int []marks = {2,4,6,8,11,23};
+
+        System.out.println(marks[3]);
+
+        for(int i=0; i<marks.length; i++){
+            System.out.println(marks[i]);
+        }
+    }
+}
